2fc3070ef23ba9755fb7e5c046e6f92330b1638c
Paave1 <129395716+Paave1@users.noreply.github.com>
2025-09-20 22:01:04 +0300

    fix(ui/card): correct forwardRef signature and TSX syntax

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,11 +1,8 @@
-import * as React from "react"
+import React, { forwardRef, HTMLAttributes } from "react"
 
 import { cn } from "@/lib/utils"
 
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
+const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
